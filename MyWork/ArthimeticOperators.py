4b8453b9935d8ccfa31fd0eda7a3d058e39c17b3
NandhiniDigajarla <nandhinidigajarla@gmail.com>
2025-09-23 02:27:56 -0500

    Updated the files

---
+++ MyWork/ArthimeticOperators.py
@@ -0,0 +1,25 @@
+print("Arithmetic Operators")
+#Arithmetic operator +
+god=100
+devil=10
+plus=god+devil
+print("The sum is",plus)
+#Arithmetic operators -
+minus=god-devil
+print("The Subtraction is",minus)
+#Arithmetic operator *
+into=god*devil
+print("The Multiplication is",into)
+#Arithmetic operator /
+division=god/devil
+print("The division is",division)
+#Arithmetic operator %
+modulas=god%devil
+print("The Modulas division is",modulas)
+#Arithmetic operator //
+floor=god//devil
+print("The floor division is",floor)
+#Arithmetic operator **
+power=god**devil
+print("The Power value is",power)
+
