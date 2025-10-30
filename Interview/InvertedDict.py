95e3796069741172dacb55670b2e3dfd3e943258
Nandhini <nandhinidigajarla@gmail.com>
2025-10-30 15:12:18 +0530

    Modified some files

---
+++ Interview/InvertedDict.py
@@ -0,0 +1,10 @@
+def invert_dict(d):
+    inverted = {}
+    for key in d:
+        value = d[key]
+        inverted[value] = key
+    return inverted
+
+# Example
+data = {'a': 1, 'b': 2, 'c': 3}
+print(invert_dict(data))
--- MyWork/example.py
+++ MyWork/example.py
@@ -11,3 +11,26 @@ print(max(Sai.keys()))'''
 
 
 print('hello this is\'prabhas\'')
+
+'''lists=[1,1,2,3,4]
+print(set(lists))'''
+
+'''def mytuple():
+    tuplemine=(1,2,3)
+    sum=0
+    for i in tuplemine:
+        sum=sum+i
+    return sum
+print(mytuple())'''
+
+'''mystring=input("Enter a string")
+result=[]
+a=" "
+b="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for i in mystring:
+    for a in mystring:
+        continue'''
+
+'''mystring="my name is nandhini"
+key=5
+print(f"My {key} name is nandhini")'''
